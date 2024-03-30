fbd41b601ff275fbb0d81cd14751968eba247d3d
medalha01 <hiperboss2009@gmail.com>
2024-03-30 18:11:32 -0300

    fix: Now using correct declarations and trait sharing

--- include/time.h
+++ include/time.h
@@ -24,42 +24,42 @@ public:
     Second now() { return RTC::seconds_since_epoch(); }
 
     Date date() { return RTC::date(); }
-    void date(const Date & d) { return RTC::date(d); }
+    void date(const Date &d) { return RTC::date(d); }
 };
-
 
 class Alarm
 {
-    friend class System;                        // for init()
-    friend class Alarm_Chronometer;             // for elapsed()
-    friend class Periodic_Thread;               // for ticks(), times(), and elapsed()
-    friend class FCFS;                          // for ticks() and elapsed()
-    friend class EDF;                           // for ticks() and elapsed()
+    friend class System;            // for init()
+    friend class Alarm_Chronometer; // for elapsed()
+    friend class Periodic_Thread;   // for ticks(), times(), and elapsed()
+    friend class FCFS;              // for ticks() and elapsed()
+    friend class EDF;               // for ticks() and elapsed()
+    friend class LLF;               // for ticks() and elapsed()
 
 private:
     typedef Timer_Common::Tick Tick;
     typedef Relative_Queue<Alarm, Tick> Queue;
 
 public:
-    Alarm(const Microsecond & time, Handler * handler, unsigned int times = 1);
+    Alarm(const Microsecond &time, Handler *handler, unsigned int times = 1);
     ~Alarm();
 
-    const Microsecond & period() const { return _time; }
-    void period(const Microsecond & p);
+    const Microsecond &period() const { return _time; }
+    void period(const Microsecond &p);
 
     void reset();
 
     static Hertz frequency() { return _timer->frequency(); }
 
-    static void delay(const Microsecond & time);
+    static void delay(const Microsecond &time);
 
 private:
     unsigned int times() const { return _times; }
 
-    static volatile Tick & elapsed() { return _elapsed; }
+    static volatile Tick &elapsed() { return _elapsed; }
 
     static Microsecond timer_period() { return 1000000 / frequency(); }
-    static Tick ticks(const Microsecond & time) { return (time + timer_period() / 2) / timer_period(); }
+    static Tick ticks(const Microsecond &time) { return (time + timer_period() / 2) / timer_period(); }
 
     static void lock() { Thread::lock(); }
     static void unlock() { Thread::unlock(); }
@@ -70,26 +70,24 @@ private:
 
 private:
     Microsecond _time;
-    Handler * _handler;
+    Handler *_handler;
     unsigned int _times;
     Tick _ticks;
     Queue::Element _link;
 
-    static Alarm_Timer * _timer;
+    static Alarm_Timer *_timer;
     static volatile Tick _elapsed;
     static Queue _request;
 };
 
-
 class Delay
 {
 public:
-    Delay(const Microsecond & time): _time(time)  { Alarm::delay(_time); }
+    Delay(const Microsecond &time) : _time(time) { Alarm::delay(_time); }
 
 private:
     Microsecond _time;
 };
-
 
 class TSC_Chronometer
 {
@@ -101,18 +99,31 @@ public:
 
     Hertz frequency() { return tsc.frequency(); }
 
-    void reset() { _start = 0; _stop = 0; }
-    void start() { if(_start == 0) _start = tsc.time_stamp(); }
-    void lap() { if(_start != 0) _stop = tsc.time_stamp(); }
+    void reset()
+    {
+        _start = 0;
+        _stop = 0;
+    }
+    void start()
+    {
+        if (_start == 0)
+            _start = tsc.time_stamp();
+    }
+    void lap()
+    {
+        if (_start != 0)
+            _stop = tsc.time_stamp();
+    }
     void stop() { lap(); }
 
     Microsecond read() { return ticks() * 1000000 / frequency(); }
 
 private:
-    Time_Stamp ticks() {
-        if(_start == 0)
+    Time_Stamp ticks()
+    {
+        if (_start == 0)
             return 0;
-        if(_stop == 0)
+        if (_stop == 0)
             return tsc.time_stamp() - _start;
         return _stop - _start;
     }
@@ -122,7 +133,6 @@ private:
     Time_Stamp _start;
     Time_Stamp _stop;
 };
-
 
 class Alarm_Chronometer
 {
@@ -134,9 +144,21 @@ public:
 
     Hertz frequency() { return Alarm::frequency(); }
 
-    void reset() { _start = 0; _stop = 0; }
-    void start() { if(_start == 0) _start = Alarm::elapsed(); }
-    void lap() { if(_start != 0) _stop = Alarm::elapsed(); }
+    void reset()
+    {
+        _start = 0;
+        _stop = 0;
+    }
+    void start()
+    {
+        if (_start == 0)
+            _start = Alarm::elapsed();
+    }
+    void lap()
+    {
+        if (_start != 0)
+            _stop = Alarm::elapsed();
+    }
     void stop() { lap(); }
 
     // The parenthesis reduces precision even more, but avoids overflow
@@ -144,10 +166,11 @@ public:
     Microsecond read() { return ticks() * (1000000 / frequency()); }
 
 private:
-    Time_Stamp ticks() {
-        if(_start == 0)
+    Time_Stamp ticks()
+    {
+        if (_start == 0)
             return 0;
-        if(_stop == 0)
+        if (_stop == 0)
             return Alarm::elapsed() - _start;
         return _stop - _start;
     }
@@ -158,7 +181,9 @@ private:
     Time_Stamp _stop;
 };
 
-class Chronometer: public IF<Traits<TSC>::enabled, TSC_Chronometer, Alarm_Chronometer>::Result {};
+class Chronometer : public IF<Traits<TSC>::enabled, TSC_Chronometer, Alarm_Chronometer>::Result
+{
+};
 
 __END_SYS
 

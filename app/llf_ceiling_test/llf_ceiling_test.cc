9333ec0b03569a1032fb434024b719e080711c4e
medalha01 <hiperboss2009@gmail.com>
2024-04-17 01:32:29 -0300

    Feat: Now with a comprehensible size

--- app/llf_ceiling_test/llf_ceiling_test.cc
+++ app/llf_ceiling_test/llf_ceiling_test.cc
@@ -2,253 +2,183 @@
 
 #include <time.h>
 #include <real-time.h>
-// #include <ceiling_utils.h>  // Make sure this includes all matrix operations and necessary headers
 
 using namespace EPOS;
-/*
-const unsigned int iterations = 100;
-const unsigned int period_a = 100;   // ms
-const unsigned int period_b = 80;    // ms
-const unsigned int period_c = 60;    // ms
-const unsigned int wcet_a = 50;      // ms
-const unsigned int wcet_b = 20;      // ms
-const unsigned int wcet_c = 10;      // ms
-const unsigned int deadline_a = 100; // ms
-const unsigned int deadline_b = 80;  // ms
-const unsigned int deadline_c = 60;  // ms*/
 
-const unsigned int iterations = 5;
-const unsigned int period_a = 2000; // ms
-const unsigned int period_b = 2200; // ms
-const unsigned int period_c = 2400; // ms
-const unsigned int period_w = 100;
-const unsigned int wcet_a = 500; // ms
-const unsigned int wcet_b = 500; // ms
-const unsigned int wcet_c = 500; // ms
-const unsigned int wcet_w = 10;
-const unsigned int deadline_a = 2000; // ms
-const unsigned int deadline_b = 2200; // ms
-const unsigned int deadline_c = 2400; // ms
-const unsigned int deadline_w = 100;
+long max(unsigned int a, unsigned int b, unsigned int c) { return ((a >= b) && (a >= c)) ? a : ((b >= a) && (b >= c) ? b : c); }
 
+// Global configuration constants
+const unsigned int iterations = 2;
+const unsigned int periods[] = {2000, 2200, 2400};   // ms for threads A, B, C
+const unsigned int wcets[] = {500, 500, 500};        // ms for threads A, B, C
+const unsigned int deadlines[] = {2000, 2200, 2400}; // ms for threads A, B, C
+const unsigned int period_w = 100;                   // ms for watcher thread
+const unsigned int wcet_w = 10;                      // ms for watcher thread
+const unsigned int deadline_w = 100;                 // ms for watcher thread
+
+// Function prototypes
 int func_a();
 int func_b();
 int func_c();
 int func_w();
-long max(unsigned int a, unsigned int b, unsigned int c) { return ((a >= b) && (a >= c)) ? a : ((b >= a) && (b >= c) ? b : c); }
 
 Chronometer chrono;
 OStream cout;
-Periodic_Thread *thread_a;
-Periodic_Thread *thread_b;
-Periodic_Thread *thread_c;
-Periodic_Thread *watcher;
+Periodic_Thread *threads[4];
+int matrices[5][12][12]; // Five matrices A-E, to simplify passing to functions
 
-int matrix_a[512][512],
-    matrix_b[512][512], matrix_c[512][512], matrix_d[512][512], matrix_e[512][512];
-
-int N = 512;
-int A = 1103515245;
-int C = 12345;
-int M = 2147483648;
-
-unsigned int rand_state = 123; // Initial seed
-// OStream cout;
-
-// Pseudo-random number generator using the Linear Congruential Generator method
-unsigned int my_rand(int seed)
-{
-    rand_state = (A * seed + C) % M;
-    return rand_state;
-}
-
-void generate_matrix(int matrix[512][512], int seed)
-{
-    int i, j;
-
-    // Populate the matrix with random numbers
-    for (i = 0; i < N; i++)
-    {
-        for (j = 0; j < N; j++)
-        {
-            matrix[i][j] = my_rand(seed) % 100; // Random numbers between 0 and 99
-        }
-    }
-}
-
-void matrix_multiply(int n, int A[512][512], int B[512][512], int C[512][512], int D[512][512], int E[512][512])
-{
-    int i, j, k;
-
-    // Initialize the result matrix to zero
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            D[i][j] = 0;
-            E[i][j] = 0;
-        }
-    }
-
-    // Multiply matrices
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            for (k = 0; k < n; k++)
-            {
-                D[i][j] += A[i][k] * B[k][j];
-            }
-        }
-    }
-
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            for (k = 0; k < n; k++)
-            {
-                E[i][j] += D[i][k] * C[k][j];
-            }
-        }
-    }
-}
-
-inline void exec(char c, unsigned int time = 0) // in miliseconds
-{
-    // Delay was not used here to prevent scheduling interference due to blocking
-    Microsecond elapsed = chrono.read() / 1000;
-
-    cout << "\n"
-         << elapsed << "\t" << c
-         << "\t[p(A)=" << thread_a->priority()
-         << ", p(B)=" << thread_b->priority()
-         << ", p(C)=" << thread_c->priority() << "]";
-
-    if (time)
-    {
-        for (Microsecond end = elapsed + time, last = end; end > elapsed; elapsed = chrono.read() / 1000)
-            if (last != elapsed)
-            {
-                cout << "\n"
-                     << elapsed << "\t" << c
-                     << "\t[p(A)=" << thread_a->priority()
-                     << ", p(B)=" << thread_b->priority()
-                     << ", p(C)=" << thread_c->priority() << "]";
-                last = elapsed;
-            }
-    }
-}
+// Helper functions
+unsigned int my_rand(unsigned int seed);
+void generate_matrix(int index, unsigned int seed);
+void matrix_multiply(int index_a, int index_b, int index_c, int index_d, int index_e);
+void exec(char c, unsigned int time = 0);
 
 int main()
 {
     cout << "Periodic Thread Component Test" << endl;
 
-    cout << "\nThis test consists in creating three periodic threads as follows:" << endl;
-    cout << "- Every " << period_a << "ms, thread A execs \"a\", waits for " << wcet_a << "ms and then execs another \"a\";" << endl;
-    cout << "- Every " << period_b << "ms, thread B execs \"b\", waits for " << wcet_b << "ms and then execs another \"b\";" << endl;
-    cout << "- Every " << period_c << "ms, thread C execs \"c\", waits for " << wcet_c << "ms and then execs another \"c\";" << endl;
-
-    cout << "Threads will now be created and I'll wait for them to finish..." << endl;
-
-    // p,d,c,act,t
-    thread_a = new Periodic_Thread(RTConf(period_a * 1000, deadline_a * 1000, wcet_a * 1000, 0, iterations), &func_a);
-    thread_b = new Periodic_Thread(RTConf(period_b * 1000, deadline_b * 1000, wcet_b * 1000, 0, iterations), &func_b);
-    thread_c = new Periodic_Thread(RTConf(period_c * 1000, deadline_c * 1000, wcet_c * 1000, 0, iterations), &func_c);
-    watcher = new Periodic_Thread(RTConf(period_w * 1000, deadline_w * 1000, wcet_w * 1000, 0, 200), &func_w);
+    threads[0] = new Periodic_Thread(RTConf(periods[0] * 1000, deadlines[0] * 1000, wcets[0] * 1000, 0, iterations), &func_a);
+    threads[1] = new Periodic_Thread(RTConf(periods[1] * 1000, deadlines[1] * 1000, wcets[1] * 1000, 0, iterations), &func_b);
+    threads[2] = new Periodic_Thread(RTConf(periods[2] * 1000, deadlines[2] * 1000, wcets[2] * 1000, 0, iterations), &func_c);
+    threads[3] = new Periodic_Thread(RTConf(period_w * 1000, deadline_w * 1000, wcet_w * 1000, 0, 200), &func_w);
 
     exec('M');
-
     chrono.start();
 
-    int status_a = thread_a->join();
-    int status_b = thread_b->join();
-    int status_c = thread_c->join();
-    watcher->join();
-
+    for (int i = 0; i < 3; ++i)
+    {
+        threads[i]->join();
+    }
+    threads[3]->join();
     chrono.stop();
-
     exec('M');
-
-    cout << "\n... done!" << endl;
-    cout << "\n\nThread A exited with status \"" << char(status_a)
-         << "\", thread B exited with status \"" << char(status_b)
-         << "\" and thread C exited with status \"" << char(status_c) << "." << endl;
-
     cout << "\nThe estimated time to run the test was "
-         << max(period_a, period_b, period_c) * iterations
+         << max(periods[0], periods[1], periods[2]) * iterations
          << " ms. The measured time was " << chrono.read() / 1000 << " ms!" << endl;
 
     cout << "I'm also done, bye!" << endl;
 
+    cout << "All threads have completed." << endl;
     return 0;
 }
 
 int func_a()
 {
-
-    // Define and initialize matrices
-    generate_matrix(matrix_a, 123);
-    generate_matrix(matrix_b, 123);
-    generate_matrix(matrix_c, 123);
-
+    generate_matrix(0, 123);
+    generate_matrix(1, 123);
+    generate_matrix(2, 123);
     do
     {
-        cout << "\nStart of A - Priority: " << thread_a->priority();
-        matrix_multiply(512, matrix_a, matrix_b, matrix_c, matrix_d, matrix_e);
-        cout << "\nEnd of A - Priority: " << thread_a->priority() << "\n";
+        cout << "A running - Priority: " << threads[0]->priority() << endl;
+        matrix_multiply(0, 1, 2, 3, 4);
+        cout << "A Finished - Priority: " << threads[0]->priority() << endl;
     } while (Periodic_Thread::wait_next());
-
     return 'A';
 }
 
 int func_b()
 {
-
-    // Define and initialize matrices
-    generate_matrix(matrix_a, 123);
-    generate_matrix(matrix_b, 123);
-    generate_matrix(matrix_c, 123);
-
+    generate_matrix(0, 123);
+    generate_matrix(1, 123);
+    generate_matrix(2, 123);
     do
     {
-        cout << "\nStart of B - Priority: " << thread_b->priority();
-        matrix_multiply(512, matrix_a, matrix_b, matrix_c, matrix_d, matrix_e);
-        cout << "\nEnd of B - Priority: " << thread_b->priority() << "\n";
+        cout << "B running - Priority: " << threads[1]->priority() << endl;
+        matrix_multiply(0, 1, 2, 3, 4);
+        cout << "B finished - Priority: " << threads[1]->priority() << endl;
     } while (Periodic_Thread::wait_next());
-
     return 'B';
 }
 
 int func_c()
 {
-
-    // Define and initialize matrices
-    generate_matrix(matrix_a, 123);
-    generate_matrix(matrix_b, 123);
-    generate_matrix(matrix_c, 123);
-
+    generate_matrix(0, 123);
+    generate_matrix(1, 123);
+    generate_matrix(2, 123);
     do
     {
-        cout << "\nStart of C - Priority: " << thread_c->priority();
-        matrix_multiply(512, matrix_a, matrix_b, matrix_c, matrix_d, matrix_e);
-        cout << "\nEnd of C - Priority: " << thread_c->priority() << "\n";
-    } while (Periodic_Thread::wait_next());
+        cout << "C running - Priority: " << threads[2]->priority() << endl;
+        matrix_multiply(0, 1, 2, 3, 4);
+        cout << "C Finished - Priority: " << threads[2]->priority() << endl;
 
+    } while (Periodic_Thread::wait_next());
     return 'C';
 }
+
 int func_w()
 {
-    exec('W');
-
     do
     {
-        cout << "\n Start of Watcher";
-        exec('W', wcet_c);
-        cout << "\n End of Watcher\n";
+        cout << "Watcher active" << endl;
+        exec('W', wcet_w);
     } while (Periodic_Thread::wait_next());
-
-    exec('W');
-
     return 'W';
+}
+
+unsigned int my_rand(unsigned int seed)
+{
+    unsigned int rand_state = seed;
+    rand_state = (1103515245 * rand_state + 12345) % 2147483648;
+    return rand_state;
+}
+
+void generate_matrix(int index, unsigned int seed)
+{
+    cout << "Starting Matrix Generation" << endl;
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            matrices[index][i][j] = my_rand(seed + i + j) % 100;
+        }
+    }
+    cout << "Ending Matrix Generation" << endl;
+}
+
+void matrix_multiply(int index_a, int index_b, int index_c, int index_d, int index_e)
+{
+    cout << "Starting Matrix Multiplication" << endl;
+
+    int n = 12;
+    // Initialize matrices D and E
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrices[index_d][i][j] = 0;
+            matrices[index_e][i][j] = 0;
+        }
+    }
+
+    // Matrix multiplication logic
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                matrices[index_d][i][j] += matrices[index_a][i][k] * matrices[index_b][k][j];
+                matrices[index_e][i][j] += matrices[index_d][i][k] * matrices[index_c][k][j];
+                cout << "Matrix"
+                     << "I" << i << "J" << j << "Value" << matrices[index_e][i][j] << endl;
+            }
+        }
+    }
+
+    cout << "Ending Matrix Multiplication" << endl;
+}
+
+void exec(char c, unsigned int time)
+{
+    Microsecond elapsed = chrono.read() / 1000;
+    cout << elapsed << "\t" << c << "\t Priority levels: [A=" << threads[0]->priority() << ", B=" << threads[1]->priority() << ", C=" << threads[2]->priority() << "]" << endl;
+
+    if (time)
+    {
+        Microsecond end = elapsed + time;
+        while (chrono.read() / 1000 < end)
+        {
+            // This loop simulates a delay
+        }
+    }
 }

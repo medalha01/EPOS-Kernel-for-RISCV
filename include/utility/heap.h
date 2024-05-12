e6bfae36638b6f5ad611f5d0a13241142563b6dc
medalha01 <hiperboss2009@gmail.com>
2024-05-12 18:04:49 -0300

    Feat: Locking Heap

--- include/utility/heap.h
+++ include/utility/heap.h
@@ -9,8 +9,14 @@
 
 __BEGIN_UTIL
 
+extern "C"
+{
+    void _lock_heap();
+    void _unlock_heap();
+}
+
 // Heap
-class Heap: private Grouping_List<char>
+class Heap : private Grouping_List<char>
 {
 protected:
     static const bool typed = Traits<System>::multiheap;
@@ -20,74 +26,98 @@ public:
     using Grouping_List<char>::size;
     using Grouping_List<char>::grouped_size;
 
-    Heap() {
+    Heap()
+    {
         db<Init, Heaps>(TRC) << "Heap() => " << this << endl;
     }
 
-    Heap(void * addr, unsigned long bytes) {
+    Heap(void *addr, unsigned long bytes)
+    {
         db<Init, Heaps>(TRC) << "Heap(addr=" << addr << ",bytes=" << bytes << ") => " << this << endl;
 
         free(addr, bytes);
     }
 
-    void * alloc(unsigned long bytes) {
+    void *alloc(unsigned long bytes)
+    {
         db<Heaps>(TRC) << "Heap::alloc(this=" << this << ",bytes=" << bytes;
+        enter_heap();
 
-        if(!bytes)
-            return 0;
+        if (!bytes)
+        {
+            leave_heap();
 
-        if(!Traits<CPU>::unaligned_memory_access)
-            while((bytes % sizeof(void *)))
-                ++bytes;
-
-        if(typed)
-            bytes += sizeof(void *);  // add room for heap pointer
-        bytes += sizeof(long);        // add room for size
-        if(bytes < sizeof(Element))
-            bytes = sizeof(Element);
-
-        Element * e = search_decrementing(bytes);
-        if(!e) {
-            out_of_memory(bytes);
             return 0;
         }
 
-        long * addr = reinterpret_cast<long *>(e->object() + e->size());
+        if (!Traits<CPU>::unaligned_memory_access)
+            while ((bytes % sizeof(void *)))
+                ++bytes;
 
-        if(typed)
+        if (typed)
+            bytes += sizeof(void *); // add room for heap pointer
+        bytes += sizeof(long);       // add room for size
+        if (bytes < sizeof(Element))
+            bytes = sizeof(Element);
+
+        Element *e = search_decrementing(bytes);
+        if (!e)
+        {
+            out_of_memory(bytes);
+            leave_heap();
+
+            return 0;
+        }
+
+        long *addr = reinterpret_cast<long *>(e->object() + e->size());
+
+        if (typed)
             *addr++ = reinterpret_cast<long>(this);
         *addr++ = bytes;
 
         db<Heaps>(TRC) << ") => " << reinterpret_cast<void *>(addr) << endl;
 
+        leave_heap();
+
         return addr;
     }
 
-    void free(void * ptr, unsigned long bytes) {
+    void free(void *ptr, unsigned long bytes)
+    {
+        enter_heap();
+
         db<Heaps>(TRC) << "Heap::free(this=" << this << ",ptr=" << ptr << ",bytes=" << bytes << ")" << endl;
 
-        if(ptr && (bytes >= sizeof(Element))) {
-            Element * e = new (ptr) Element(reinterpret_cast<char *>(ptr), bytes);
-            Element * m1, * m2;
+        if (ptr && (bytes >= sizeof(Element)))
+        {
+            Element *e = new (ptr) Element(reinterpret_cast<char *>(ptr), bytes);
+            Element *m1, *m2;
             insert_merging(e, &m1, &m2);
         }
+        leave_heap();
     }
 
-    static void typed_free(void * ptr) {
-        long * addr = reinterpret_cast<long *>(ptr);
+    static void typed_free(void *ptr)
+    {
+
+        long *addr = reinterpret_cast<long *>(ptr);
         unsigned long bytes = *--addr;
-        Heap * heap = reinterpret_cast<Heap *>(*--addr);
+        Heap *heap = reinterpret_cast<Heap *>(*--addr);
         heap->free(addr, bytes);
     }
 
-    static void untyped_free(Heap * heap, void * ptr) {
-        long * addr = reinterpret_cast<long *>(ptr);
+    static void untyped_free(Heap *heap, void *ptr)
+    {
+        long *addr = reinterpret_cast<long *>(ptr);
         unsigned long bytes = *--addr;
         heap->free(addr, bytes);
     }
 
 private:
     void out_of_memory(unsigned long bytes);
+
+    void enter_heap() { _lock_heap(); }
+    void leave_heap() { _unlock_heap(); }
 };
 
 __END_UTIL

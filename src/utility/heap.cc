de78ed86bbb4a0b14e5a92b5e9dc5b57c70686ec
scarpart <aenartzr@gmail.com>
2024-05-13 08:22:14 -0300

    feat: heap_wrapper e n tem mais deadlock eu acho na heap

--- src/utility/heap.cc
+++ src/utility/heap.cc
@@ -4,7 +4,7 @@
 
 __BEGIN_UTIL
 
-void Heap::out_of_memory(unsigned long bytes)
+void Simple_Heap::out_of_memory(unsigned long bytes)
 {
     db<Heaps, System>(ERR) << "Heap::alloc(this=" << this << "): out of memory while allocating " << bytes << " bytes!" << endl;
 }

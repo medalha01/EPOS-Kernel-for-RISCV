8788c857a4ee6729aa09e5588f04899e61198083
medalha01 <hiperboss2009@gmail.com>
2024-05-14 20:33:02 -0300

    Feat: Tamo Jantando

--- app/philosophers_dinner/philosophers_dinner_traits.h
+++ app/philosophers_dinner/philosophers_dinner_traits.h
@@ -6,7 +6,8 @@
 __BEGIN_SYS
 
 // Build
-template<> struct Traits<Build>: public Traits_Tokens
+template <>
+struct Traits<Build> : public Traits_Tokens
 {
     // Basic configuration
     static const unsigned int SMOD = LIBRARY;
@@ -20,69 +21,75 @@ template<> struct Traits<Build>: public Traits_Tokens
     // Default flags
     static const bool enabled = true;
     static const bool monitored = true;
-    static const bool debugged = true;
+    static const bool debugged = false;
     static const bool hysterically_debugged = false;
-        static const bool trace   = true;
-
+    static const bool trace = false;
 };
-
 
 // Utilities
-template<> struct Traits<Debug>: public Traits<Build>
+template <>
+struct Traits<Debug> : public Traits<Build>
 {
-    static const bool debugged = true;
+    static const bool debugged = false;
 
-    static const bool error   = true;
+    static const bool error = true;
     static const bool warning = true;
-    static const bool info    = false;
-    static const bool trace   = true;
+    static const bool info = false;
+    static const bool trace = false;
 };
 
-template<> struct Traits<Lists>: public Traits<Build>
+template <>
+struct Traits<Lists> : public Traits<Build>
 {
     static const bool debugged = hysterically_debugged;
 };
 
-template<> struct Traits<Spin>: public Traits<Build>
+template <>
+struct Traits<Spin> : public Traits<Build>
 {
     static const bool debugged = hysterically_debugged;
 };
 
-template<> struct Traits<Heaps>: public Traits<Build>
+template <>
+struct Traits<Heaps> : public Traits<Build>
 {
     static const bool debugged = hysterically_debugged;
 };
 
-template<> struct Traits<Observers>: public Traits<Build>
+template <>
+struct Traits<Observers> : public Traits<Build>
 {
     // Some observed objects are created before initializing the Display
     // Enabling debug may cause trouble in some Machines
     static const bool debugged = false;
 };
 
-
 // System Parts (mostly to fine control debugging)
-template<> struct Traits<Boot>: public Traits<Build>
+template <>
+struct Traits<Boot> : public Traits<Build>
 {
 };
 
-template<> struct Traits<Setup>: public Traits<Build>
+template <>
+struct Traits<Setup> : public Traits<Build>
 {
 };
 
-template<> struct Traits<Init>: public Traits<Build>
+template <>
+struct Traits<Init> : public Traits<Build>
 {
 };
 
-template<> struct Traits<Framework>: public Traits<Build>
+template <>
+struct Traits<Framework> : public Traits<Build>
 {
 };
 
-template<> struct Traits<Aspect>: public Traits<Build>
+template <>
+struct Traits<Aspect> : public Traits<Build>
 {
     static const bool debugged = hysterically_debugged;
 };
-
 
 __END_SYS
 
@@ -92,23 +99,24 @@ __END_SYS
 
 __BEGIN_SYS
 
-
 // API Components
-template<> struct Traits<Application>: public Traits<Build>
+template <>
+struct Traits<Application> : public Traits<Build>
 {
     static const unsigned int STACK_SIZE = Traits<Machine>::STACK_SIZE;
     static const unsigned int HEAP_SIZE = Traits<Machine>::HEAP_SIZE;
     static const unsigned int MAX_THREADS = Traits<Machine>::MAX_THREADS;
 };
 
-template<> struct Traits<System>: public Traits<Build>
+template <>
+struct Traits<System> : public Traits<Build>
 {
     static const bool multithread = (Traits<Application>::MAX_THREADS > 1);
     static const bool multiheap = Traits<Scratchpad>::enabled;
-	static const bool multicore = true;
+    static const bool multicore = true;
 
     static const unsigned long LIFE_SPAN = 1 * YEAR; // s
-    static const unsigned int DUTY_CYCLE = 1000000; // ppm
+    static const unsigned int DUTY_CYCLE = 1000000;  // ppm
 
     static const bool reboot = true;
 
@@ -116,7 +124,8 @@ template<> struct Traits<System>: public Traits<Build>
     static const unsigned int HEAP_SIZE = (Traits<Application>::MAX_THREADS + 1) * Traits<Application>::STACK_SIZE;
 };
 
-template<> struct Traits<Thread>: public Traits<Build>
+template <>
+struct Traits<Thread> : public Traits<Build>
 {
     static const bool enabled = Traits<System>::multithread;
     static const bool trace_idle = hysterically_debugged;
@@ -127,24 +136,33 @@ template<> struct Traits<Thread>: public Traits<Build>
     static const unsigned int QUANTUM = 10000; // us
 };
 
-template<> struct Traits<Scheduler<Thread>>: public Traits<Build>
+template <>
+struct Traits<Scheduler<Thread>> : public Traits<Build>
 {
     static const bool debugged = Traits<Thread>::trace_idle || hysterically_debugged;
 };
 
-template<> struct Traits<Synchronizer>: public Traits<Build>
+template <>
+struct Traits<Synchronizer> : public Traits<Build>
 {
     static const bool enabled = Traits<System>::multithread;
 };
 
-template<> struct Traits<Alarm>: public Traits<Build>
+template <>
+struct Traits<Alarm> : public Traits<Build>
 {
     static const bool visible = hysterically_debugged;
 };
 
-template<> struct Traits<Address_Space>: public Traits<Build> {};
+template <>
+struct Traits<Address_Space> : public Traits<Build>
+{
+};
 
-template<> struct Traits<Segment>: public Traits<Build> {};
+template <>
+struct Traits<Segment> : public Traits<Build>
+{
+};
 
 __END_SYS
 

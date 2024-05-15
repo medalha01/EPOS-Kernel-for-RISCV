f6bdd3f98ced4cb4216cd43e073eded1a7374bd7
medalha01 <hiperboss2009@gmail.com>
2024-05-14 21:07:58 -0300

    Feat: 4 Cores?

--- tests/scheduler_llf_test/scheduler_llf_test_traits.h
+++ tests/scheduler_llf_test/scheduler_llf_test_traits.h
@@ -14,7 +14,7 @@ struct Traits<Build> : public Traits_Tokens
     static const unsigned int ARCHITECTURE = RV64;
     static const unsigned int MACHINE = RISCV;
     static const unsigned int MODEL = SiFive_U;
-    static const unsigned int CPUS = 2;
+    static const unsigned int CPUS = 4;
     static const unsigned int NETWORKING = STANDALONE;
     static const unsigned int EXPECTED_SIMULATION_TIME = 60; // s (0 => not simulated)
 

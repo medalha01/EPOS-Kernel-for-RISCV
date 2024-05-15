970e3fd260cdfc02efcf0ad8aa5163fffa6f0fe9
Isac Martins <isacmartins.223@gmail.com>
2024-05-15 00:40:47 -0300

    Revert "Feat: Riscv timer improved docs"
    This reverts commit 2a7b1a801990af8a204d6503393dbd59f22ff51b.

--- include/machine/riscv/riscv_timer.h
+++ include/machine/riscv/riscv_timer.h
@@ -38,171 +38,126 @@ public:
     static const Hertz CLOCK = Traits<Timer>::CLOCK;
 
 protected:
-    /**
-     * @brief Constructs a Timer object.
-     *
-     * @param channel The channel for the timer (SCHEDULER or ALARM).
-     * @param frequency The frequency for the timer.
-     * @param handler The handler function for the timer interrupts.
-     * @param retrigger Whether the timer should retrigger automatically.
-     */
-    Timer(unsigned int channel, Hertz frequency, Handler handler, bool retrigger = true) 
-        : _channel(channel),
-          _initial(FREQUENCY / frequency),
-          _retrigger(retrigger),
-          _handler(handler)
-    {
-        db<Timer>(TRC) << "Initializing Timer: frequency=" << frequency << ", handler=" 
-                       << reinterpret_cast<void *>(handler) << ", channel=" << channel 
-                       << ", initial count=" << _initial << endl;
+	Timer(unsigned int channel, Hertz frequency, Handler handler, bool retrigger = true) 
+		: _channel(channel),
+		  _initial(FREQUENCY / frequency),
+		  _retrigger(retrigger),
+		  _handler(handler)
+	{
+		//db<Thread>(WRN) << "@@@TIMER CONS@@@ frequency = " << frequency  << " - --- -AAAAAAAAAA" << endl;
+		//db<Thread>(WRN) << "@@@TIMER CONS@@@ FREQUENCY / frequency = " << FREQUENCY / frequency << endl;
 
-        if (_initial && (channel < CHANNELS) && !_channels[channel])
-        {
-            _channels[channel] = this;
-        }
-        else
-        {
-            db<Timer>(TRC) << "Failed to install Timer on channel " << channel << endl;
-        }
+		//db<Thread>(WRN) << "--Timer(f=" << frequency
+		//	<< ",h=" << reinterpret_cast<void *>(handler)
+		//	<< ",ch=" << channel << ") => {count=" << _initial << "}"
+		//	<< endl;
 
-        for (unsigned int i = 0; i < Traits<Machine>::CPUS; i++)
-        {
-            _current[i] = _initial;
-        }
+		if (_initial && (channel < CHANNELS) && !_channels[channel])
+		{
+			db<Thread>(WRN) << "timer: no if _initial && channel\n\n" << endl;
+			_channels[channel] = this;
+		}
+		else
+		{
+			db<Thread>(WRN) << "timer: NO ELSE\n\n" << endl;
+			db<Timer>(WRN) << "Timer not installed!" << endl;
+		}
 
-        db<Timer>(TRC) << "Timer initialization complete for channel " << channel << endl;
-    }
+		for (unsigned int i = 0; i < Traits<Machine>::CPUS; i++)
+		{
+			db<Thread>(WRN) << "timer: current[" << i << "] = initial\n\n" << endl;
+			_current[i] = _initial;
+		}
+
+		db<Thread>(WRN) << "\n\ntimer:cabou" << endl;
+	}
 
 public:
-    /**
-     * @brief Destructs the Timer object.
-     */
-    ~Timer() {
-        db<Timer>(TRC) << "Destroying Timer: frequency=" << frequency() << ", handler=" 
-                       << reinterpret_cast<void *>(_handler) << ", channel=" << _channel 
-                       << ", initial count=" << _initial << endl;
+	~Timer() {
+		db<Timer>(TRC) << "~Timer(f=" << frequency()
+			<< ",h=" << reinterpret_cast<void *>(_handler)
+			<< ",ch=" << _channel << ") => {count=" << _initial
+			<< "}" << endl;
 
-        _channels[_channel] = 0;
-    }
+		_channels[_channel] = 0;
+	}
 
-    /**
-     * @brief Reads the current tick count for the CPU.
-     *
-     * @return The current tick count.
-     */
     Tick read() { return _current[CPU::id()]; }
 
-    /**
-     * @brief Restarts the timer and returns the percentage of time elapsed.
-     *
-     * @return The percentage of time elapsed since the timer was last started.
-     */
-    int restart() {
-        int percentage = _current[CPU::id()] * 100 / _initial;
-        _current[CPU::id()] = _initial;
-        return percentage;
-    }
+	int restart() {
+		// TODO: change to <Timer> again
+		//
+		//db<Thread>(WRN) << "@@@TIMER _initialjkfldsajfldsajfedsjlkfdsajlkfdsajlkfdsajlksafdjlflsdaj" << endl; 
+		//db<Thread>(WRN) << "@@@TIMER _initial = " << _initial << endl;
+		//db<Thread>(WRN) << "@@@TIMER FREQUENCY = " << FREQUENCY << endl;
 
-    /**
-     * @brief Resets the timer configuration.
-     */
+		//db<Thread>(WRN) << "@@@TIMER::restart() => {f=" << frequency() << endl;
+			//<< ",h=" << reinterpret_cast<void *>(_handler)
+			//<< ",count=" << _current[CPU::id()] << "}" << endl;
+
+		//db<Thread>(WRN) << "@@@TIMER: ANTES DO PERCENTAGE" << endl;
+
+		int percentage = _current[CPU::id()] * 100 / _initial;
+
+		//db<Thread>(WRN) << "@@@TIMER: LOGO DEPOIS DO PERCENTAGE" << endl;
+		
+		_current[CPU::id()] = _initial;
+
+		//db<Thread>(WRN) << "@@@TIMER: LOGO DEPOIS DE SETAR O CPU_ID = " << _initial << endl;
+
+		return percentage;
+	}
+
     static void reset() { config(FREQUENCY); }
-
-    /**
-     * @brief Enables the timer.
-     */
     static void enable() {}
-
-    /**
-     * @brief Disables the timer.
-     */
     static void disable() {}
 
-    /**
-     * @brief Gets the frequency of the timer.
-     *
-     * @return The timer frequency.
-     */
     Hertz frequency() const { return (FREQUENCY / _initial); }
-
-    /**
-     * @brief Sets the frequency of the timer.
-     *
-     * @param f The new frequency.
-     */
     void frequency(Hertz f)
     {
         _initial = FREQUENCY / f;
         reset();
     }
 
-    /**
-     * @brief Sets the handler function for the timer.
-     *
-     * @param handler The new handler function.
-     */
     void handler(Handler handler) { _handler = handler; }
 
 private:
-    /**
-     * @brief Configures the timer with the given frequency.
-     *
-     * @param frequency The frequency to configure.
-     */
     static void config(Hertz frequency) 
-    { 
-        mtimecmp(mtime() + (CLOCK / frequency)); 
-    }
+	{ 
+		//db<Thread>(WRN) << "TIMER config antes do mtimecmp" << endl;
+		mtimecmp(mtime() + (CLOCK / frequency)); 
+		//db<Thread>(WRN) << "TIMER config depois do mtimecmp" << endl;
+	}
 
-    /**
-     * @brief Handles timer interrupts.
-     *
-     * @param i The interrupt ID.
-     */
     static void int_handler(Interrupt_Id i);
 
-    /**
-     * @brief Initializes the timer system.
-     */
     static void init();
 
 protected:
-    unsigned int _channel;                 ///< Timer channel.
-    Tick _initial;                         ///< Initial tick count.
-    bool _retrigger;                       ///< Whether the timer should retrigger automatically.
-    volatile Tick _current[Traits<Machine>::CPUS];  ///< Current tick count for each CPU.
-    Handler _handler;                      ///< Handler function for timer interrupts.
+    unsigned int _channel;
+    Tick _initial;
+    bool _retrigger;
+    volatile Tick _current[Traits<Machine>::CPUS];
+    Handler _handler;
 
-    static Timer *_channels[CHANNELS];     ///< Timer channels.
+    static Timer *_channels[CHANNELS];
 };
 
 // Timer used by Thread::Scheduler
 class Scheduler_Timer : public Timer
 {
 public:
-    /**
-     * @brief Constructs a Scheduler_Timer object.
-     *
-     * @param quantum The time quantum for the scheduler.
-     * @param handler The handler function for the scheduler timer.
-     */
-    Scheduler_Timer(Microsecond quantum, Handler handler) 
-        : Timer(SCHEDULER, 1000000 / quantum, handler) 
-    {
-        db<Timer>(TRC) << "Scheduler_Timer initialized: quantum=" << quantum 
-                       << ", initial count=" << 1000000 / quantum << endl;
-    }
+    Scheduler_Timer(Microsecond quantum, Handler handler) : Timer(SCHEDULER, 1000000 / quantum, handler) 
+	{
+		db<Thread>(WRN) << "@@@SCHEDTIMER@@@ quantum = " << quantum << endl;
+		db<Thread>(WRN) << "@@@SCHEDTIMER@@@ _initial = " << 1000000 / quantum << endl;
+	}
 };
 
 // Timer used by Alarm
 class Alarm_Timer : public Timer
 {
 public:
-    /**
-     * @brief Constructs an Alarm_Timer object.
-     *
-     * @param handler The handler function for the alarm timer.
-     */
     Alarm_Timer(Handler handler) : Timer(ALARM, FREQUENCY, handler) {}
 };
 

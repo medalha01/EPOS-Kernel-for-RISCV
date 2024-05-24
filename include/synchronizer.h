0f60104c518ea5442bf44add612e16ed3636224b
Isac Martins <isacmartins.223@gmail.com>
2024-05-24 00:01:27 -0300

    Feat:base changes for support

--- include/synchronizer.h
+++ include/synchronizer.h
@@ -16,54 +16,61 @@ protected:
 
 protected:
     Synchronizer_Common() {}
-    ~Synchronizer_Common() {
+    ~Synchronizer_Common()
+    {
+
         Thread::lock();
-        while(!_granted.empty()) {
-            Queue::Element * e = _granted.remove();
-            if(e)
+
+        while (!_granted.empty())
+        {
+            Queue::Element *e = _granted.remove();
+            if (e)
                 delete e;
         }
-        if(!_waiting.empty())
+
+        if (!_waiting.empty())
+        {
+
             db<Synchronizer>(WRN) << "~Synchronizer(this=" << this << ") called with active blocked clients!" << endl;
+        }
+
         wakeup_all();
+
         Thread::unlock();
     }
 
     // Atomic operations
-    bool tsl(volatile bool & lock) { return CPU::tsl(lock); }
-    long finc(volatile long & number) { return CPU::finc(number); }
-    long fdec(volatile long & number) { return CPU::fdec(number); }
+    bool tsl(volatile bool &lock) { return CPU::tsl(lock); }
+    long finc(volatile long &number) { return CPU::finc(number); }
+    long fdec(volatile long &number) { return CPU::fdec(number); }
 
     // Thread operations
-    void lock_for_acquiring() 
-	{
-      Thread::lock();
-      Thread::prioritize(&_granted);
+    void lock_for_acquiring()
+    {
+        Thread::lock();
+        Thread::prioritize(&_granted);
     }
 
-	void unlock_for_acquiring() 
-	{
-		_granted.insert(new (SYSTEM) Queue::Element(Thread::running()));
-		Thread::unlock();
-	}
+    void unlock_for_acquiring()
+    {
+        _granted.insert(new (SYSTEM) Queue::Element(Thread::running()));
+        Thread::unlock();
+    }
 
-	void lock_for_releasing() {
-		Thread::lock();
-		Queue::Element *e = _granted.remove();
+    void lock_for_releasing()
+    {
+        Thread::lock();
+        Queue::Element *e = _granted.remove();
+        if (e)
+            delete e;
+        Thread::deprioritize(&_granted);
+        Thread::deprioritize(&_waiting);
+    }
 
-		if (e)
-		{
-			delete e;
-		}
-
-		Thread::deprioritize(&_granted);
-		Thread::deprioritize(&_waiting);
-	}
-
-	void unlock_for_releasing() 
-	{
-		Thread::unlock(); 
-	}
+    void unlock_for_releasing()
+    {
+        Thread::unlock();
+    }
 
     void sleep() { Thread::sleep(&_waiting); }
     void wakeup() { Thread::wakeup(&_waiting); }
@@ -74,8 +81,7 @@ protected:
     Queue _granted;
 };
 
-
-class Mutex: protected Synchronizer_Common
+class Mutex : protected Synchronizer_Common
 {
 public:
     Mutex();
@@ -88,8 +94,7 @@ private:
     volatile bool _locked;
 };
 
-
-class Semaphore: protected Synchronizer_Common
+class Semaphore : protected Synchronizer_Common
 {
 public:
     Semaphore(long v = 1);
@@ -102,10 +107,9 @@ private:
     volatile long _value;
 };
 
-
 // This is actually no Condition Variable
 // check http://www.cs.duke.edu/courses/spring01/cps110/slides/sem/sld002.htm
-class Condition: protected Synchronizer_Common
+class Condition : protected Synchronizer_Common
 {
 public:
     Condition();
@@ -116,44 +120,43 @@ public:
     void broadcast();
 };
 
-
 // An event handler that triggers a mutex (see handler.h)
-class Mutex_Handler: public Handler
+class Mutex_Handler : public Handler
 {
 public:
-    Mutex_Handler(Mutex * h) : _handler(h) {}
+    Mutex_Handler(Mutex *h) : _handler(h) {}
     ~Mutex_Handler() {}
 
     void operator()() { _handler->unlock(); }
 
 private:
-    Mutex * _handler;
+    Mutex *_handler;
 };
 
 // An event handler that triggers a semaphore (see handler.h)
-class Semaphore_Handler: public Handler
+class Semaphore_Handler : public Handler
 {
 public:
-    Semaphore_Handler(Semaphore * h) : _handler(h) {}
+    Semaphore_Handler(Semaphore *h) : _handler(h) {}
     ~Semaphore_Handler() {}
 
     void operator()() { _handler->v(); }
 
 private:
-    Semaphore * _handler;
+    Semaphore *_handler;
 };
 
 // An event handler that triggers a condition variable (see handler.h)
-class Condition_Handler: public Handler
+class Condition_Handler : public Handler
 {
 public:
-    Condition_Handler(Condition * h) : _handler(h) {}
+    Condition_Handler(Condition *h) : _handler(h) {}
     ~Condition_Handler() {}
 
     void operator()() { _handler->signal(); }
 
 private:
-    Condition * _handler;
+    Condition *_handler;
 };
 
 __END_SYS

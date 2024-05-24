56ebefffe865dfbe72293499dae435b732107f4b
Isac Martins <isacmartins.223@gmail.com>
2024-05-24 15:34:11 -0300

    Feat: Update to the state on management

--- include/process.h
+++ include/process.h
@@ -29,6 +29,7 @@ class Thread
     friend class IC;                  // for link() for priority ceiling
     friend void ::_lock_heap();       // for lock()
     friend void ::_unlock_heap();     // for unlock()
+    friend class CpuLookTable;
 
 protected:
     static const bool preemptive = Traits<Thread>::Criterion::preemptive;
@@ -224,6 +225,120 @@ public:
 
 private:
     Thread *_handler;
+};
+
+class CpuLookTable
+{
+    friend class Thread;
+    friend class Alarm;
+    friend class Mutex;
+    friend class Condition;
+    friend class Semaphore;
+    friend class Segment;
+
+    typedef Traits<Thread>::Criterion Criterion;
+
+private:
+    Criterion *threads_criterion_on_execution[Traits<Build>::CPUS];
+    Thread *running_thread_by_core[Traits<Build>::CPUS];
+
+public:
+    CpuLookTable()
+    {
+        // Initialize arrays
+        for (unsigned int i = 0; i < Traits<Build>::CPUS; ++i)
+        {
+            running_thread_by_core[i] = nullptr;
+            threads_criterion_on_execution[i] = nullptr;
+        }
+    }
+
+    Thread *get_thread_on_cpu(unsigned int cpu_id)
+    {
+        assert(cpu_id < Traits<Build>::CPUS); // Ensure valid cpu_id
+        return running_thread_by_core[cpu_id];
+    }
+
+    Criterion *get_priority_on_cpu(unsigned int cpu_id)
+    {
+        assert(cpu_id < Traits<Build>::CPUS); // Ensure valid cpu_id
+        return threads_criterion_on_execution[cpu_id];
+    }
+
+    void set_thread_on_cpu_table(Thread *running, unsigned int id)
+    {
+        assert(id < Traits<Build>::CPUS); // Ensure valid cpu_id
+        running_thread_by_core[id] = running;
+        threads_criterion_on_execution[id] = &running->criterion();
+    }
+    void set_thread_on_cpu_table(Thread *running)
+    {
+        unsigned int id = CPU::id();
+
+        running_thread_by_core[id] = running;
+        threads_criterion_on_execution[id] = &running->criterion();
+    }
+
+    unsigned int get_cpu_with_lowest_priority()
+    {
+        int min = Thread::IDLE;
+        unsigned int lowest_priority_cpu = 0;
+        for (unsigned int i = 0; i < Traits<Build>::CPUS; i++)
+        {
+            if (min > *threads_criterion_on_execution[i])
+            {
+                min = *threads_criterion_on_execution[i];
+                lowest_priority_cpu = i;
+            }
+        }
+
+        return lowest_priority_cpu;
+    }
+
+    unsigned int get_idle_cpu()
+    {
+        for (unsigned int i = 0; i < Traits<Build>::CPUS; i++)
+        {
+            if ((running_thread_by_core[i] == nullptr) or (*threads_criterion_on_execution[i] == Thread::IDLE))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    void clear_cpu(unsigned int cpu_id)
+    {
+        assert(cpu_id < Traits<Build>::CPUS); // Ensure valid cpu_id
+        running_thread_by_core[cpu_id] = nullptr;
+        threads_criterion_on_execution[cpu_id] = nullptr;
+    }
+    // Check if a specific CPU is idle
+    bool is_cpu_idle(unsigned int cpu_id)
+    {
+        assert(cpu_id < Traits<Build>::CPUS); // Ensure valid cpu_id
+        return (running_thread_by_core[cpu_id] == nullptr) ||
+               (threads_criterion_on_execution[cpu_id] && *threads_criterion_on_execution[cpu_id] == Thread::IDLE);
+    }
+
+    // Get the total number of CPUs
+    unsigned int get_total_cpus()
+    {
+        return Traits<Build>::CPUS;
+    }
+
+    // Check if all CPUs are idle
+    bool are_all_cpus_idle()
+    {
+        for (unsigned int i = 0; i < Traits<Build>::CPUS; ++i)
+        {
+            if (!is_cpu_idle(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 };
 
 __END_SYS

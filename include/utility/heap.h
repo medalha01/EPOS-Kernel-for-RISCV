4b7dc12c0bc986c83898f09468b351d797f41bcf
Isac Martins <isacmartins.223@gmail.com>
2024-05-24 00:35:31 -0300

    Feat: More changes, to the state of alarm and timer lock, also locking the heap

--- include/utility/heap.h
+++ include/utility/heap.h
@@ -9,125 +9,139 @@
 
 __BEGIN_UTIL
 
+extern "C"
+{
+    void _lock_heap();
+    void _unlock_heap();
+}
+
 // Heap
-class Heap: private Grouping_List<char>
+class Heap : private Grouping_List<char>
 {
 protected:
     static const bool typed = Traits<System>::multiheap;
 
 public:
-	typedef Grouping_List<char> Base;
+    using Grouping_List<char>::empty;
+    using Grouping_List<char>::size;
+    using Grouping_List<char>::grouped_size;
 
-    using Base::empty;
-    using Base::size;
-    using Base::grouped_size;
-
-    Heap() 
-	{
+    Heap()
+    {
         db<Init, Heaps>(TRC) << "Heap() => " << this << endl;
     }
 
-    Heap(void * addr, unsigned long bytes) 
-	{
+    Heap(void *addr, unsigned long bytes)
+    {
         db<Init, Heaps>(TRC) << "Heap(addr=" << addr << ",bytes=" << bytes << ") => " << this << endl;
 
         free(addr, bytes);
     }
 
-	unsigned long size() 
-	{
-		lock();
-		auto temp = Base::size();
-		unlock();
-		return temp;
-	}
+    void *alloc(unsigned long bytes)
+    {
+        enter_heap();
+        db<Heaps, Thread>(WRN) << "Heap::alloc(this=" << this << ",bytes=" << bytes;
 
-	void * alloc(unsigned long bytes) 
-	{
-		lock();
-		auto temp = _helper_alloc(bytes);
-		unlock();
-		return temp;
-	}
-
-	void free(void * ptr, unsigned long bytes) 
-	{
-		lock();
-		_helper_free(ptr, bytes);
-		unlock();
-	}
-
-    static void typed_free(void * ptr) 
-	{
-        long * addr = reinterpret_cast<long *>(ptr);
-        unsigned long bytes = *--addr;
-        Heap * heap = reinterpret_cast<Heap *>(*--addr);
-        heap->free(addr, bytes);
-    }
-
-    static void untyped_free(Heap * heap, void * ptr) 
-	{
-        long * addr = reinterpret_cast<long *>(ptr);
-        unsigned long bytes = *--addr;
-        heap->free(addr, bytes);
-    }
-
-private:
-	//Spin _spin;
-	Simple_Spin _spin;
-
-	void lock()   { _spin.acquire(); }
-	void unlock() { _spin.release(); }
-
-    void out_of_memory(unsigned long bytes);
-
-    //void * alloc(unsigned long bytes) {
-    void * _helper_alloc(unsigned long bytes)
-	{
-        db<Heaps>(TRC) << "Heap::alloc(this=" << this << ",bytes=" << bytes;
-
-        if(!bytes)
-            return 0;
-
-        if(!Traits<CPU>::unaligned_memory_access)
-            while((bytes % sizeof(void *)))
-                ++bytes;
-
-        if(typed)
-            bytes += sizeof(void *);  // add room for heap pointer
-        bytes += sizeof(long);        // add room for size
-        if(bytes < sizeof(Element))
-            bytes = sizeof(Element);
-
-        Element * e = search_decrementing(bytes);
-        if(!e) 
-		{
-            out_of_memory(bytes);
+        if (!bytes)
+        {
+            leave_heap();
             return 0;
         }
 
-        long * addr = reinterpret_cast<long *>(e->object() + e->size());
+        if (!Traits<CPU>::unaligned_memory_access)
+        {
+            while ((bytes % sizeof(void *)))
+            {
+                ++bytes;
+            }
+        }
 
-        if(typed)
+        if (typed)
+        {
+            bytes += sizeof(void *); // add room for heap pointer
+        }
+        bytes += sizeof(long); // add room for size
+        if (bytes < sizeof(Element))
+        {
+            bytes = sizeof(Element);
+        }
+
+        Element *e = search_decrementing(bytes);
+        if (!e)
+        {
+
+            out_of_memory(bytes);
+            leave_heap();
+            return 0;
+        }
+
+        long *addr = reinterpret_cast<long *>(e->object() + e->size());
+
+        if (typed)
+        {
             *addr++ = reinterpret_cast<long>(this);
+        }
         *addr++ = bytes;
 
         db<Heaps>(TRC) << ") => " << reinterpret_cast<void *>(addr) << endl;
 
+        leave_heap();
         return addr;
     }
 
-    void _helper_free(void * ptr, unsigned long bytes) 
-	{
+    void free(void *ptr, unsigned long bytes)
+    {
+        enter_heap();
+
         db<Heaps>(TRC) << "Heap::free(this=" << this << ",ptr=" << ptr << ",bytes=" << bytes << ")" << endl;
 
-        if(ptr && (bytes >= sizeof(Element))) 
-		{
-            Element * e = new (ptr) Element(reinterpret_cast<char *>(ptr), bytes);
-            Element * m1, * m2;
+        if (ptr && (bytes >= sizeof(Element)))
+        {
+            Element *e = new (ptr) Element(reinterpret_cast<char *>(ptr), bytes);
+            Element *m1, *m2;
+            insert_merging(e, &m1, &m2);
+        }
+
+        leave_heap();
+    }
+
+    void non_lock_free(void *ptr, unsigned long bytes)
+    {
+
+        db<Heaps>(TRC) << "Heap::free(this=" << this << ",ptr=" << ptr << ",bytes=" << bytes << ")" << endl;
+
+        if (ptr && (bytes >= sizeof(Element)))
+        {
+            Element *e = new (ptr) Element(reinterpret_cast<char *>(ptr), bytes);
+            Element *m1, *m2;
             insert_merging(e, &m1, &m2);
         }
     }
+
+    static void typed_free(void *ptr)
+    {
+        long *addr = reinterpret_cast<long *>(ptr);
+        unsigned long bytes = *--addr;
+        Heap *heap = reinterpret_cast<Heap *>(*--addr);
+
+        heap->non_lock_free(addr, bytes);
+    }
+
+    static void untyped_free(Heap *heap, void *ptr)
+    {
+        enter_heap();
+        long *addr = reinterpret_cast<long *>(ptr);
+        unsigned long bytes = *--addr;
+        heap->non_lock_free(addr, bytes);
+        leave_heap();
+    }
+
+private:
+    void out_of_memory(unsigned long bytes);
+
+    static void enter_heap() { _lock_heap(); }
+    static void leave_heap() { _unlock_heap(); }
 };
 
 __END_UTIL
--- src/api/alarm_init.cc
+++ src/api/alarm_init.cc
@@ -7,10 +7,12 @@ __BEGIN_SYS
 
 void Alarm::init()
 {
-    db<Init, Alarm>(TRC) << "Alarm::init()" << endl;
+    if (CPU::is_bootstrap())
+    {
+        db<Init, Alarm>(TRC) << "Alarm::init()" << endl;
 
-	db<Thread>(WRN) << "@@@ALARMINIT aqui pelo bootstrap " << endl;
-    _timer = new (SYSTEM) Alarm_Timer(handler);
+        _timer = new (SYSTEM) Alarm_Timer(handler);
+    }
 }
 
 __END_SYS

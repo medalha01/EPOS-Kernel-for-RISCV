23dbbaa69ef90e7467d43d6b898aef6f9701705b
scarpart <aenartzr@gmail.com>
2024-05-26 22:27:48 -0300

    checkpoint dnv

--- include/scheduler.h
+++ include/scheduler.h
@@ -285,7 +285,7 @@ public:
 
 public:
     LLF(int p = APERIODIC) : RT_Common(p) {}
-    LLF(Microsecond p, Microsecond d = SAME, Microsecond c = UNKNOWN);
+    LLF(Microsecond p, Microsecond d = SAME, Microsecond c = UNKNOWN, unsigned int cpu = ANY);
 
     void handle(Event event);
 };
@@ -298,12 +298,34 @@ public:
 	
 public: 
     GLLF(int p = APERIODIC): LLF(p) {}
-    GLLF(Microsecond p, Microsecond d = SAME, Microsecond c = UNKNOWN) 
-		: LLF(p, d, c) { }; 
+    GLLF(Microsecond p, Microsecond d = SAME, Microsecond c = UNKNOWN, unsigned int cpu = ANY) 
+		: LLF(p, d, c, cpu) { }; 
 
     unsigned int queue() const { return current_head(); }
     void queue(unsigned int q) {}
     static unsigned int current_head() { return CPU::id(); }
+};
+
+// Partitioned Least Laxity First 
+class PLLF: public LLF
+{
+public:
+    static const unsigned int QUEUES = Traits<Machine>::CPUS;
+
+public:
+	PLLF(int p = APERIODIC) 
+		: LLF(p), _queue(((_priority == IDLE) || (_priority == MAIN)) ? CPU::id() : 0) {};
+
+	PLLF(const Microsecond & d, const Microsecond & p = SAME, const Microsecond & c = UNKNOWN, unsigned int cpu = ANY)
+		: LLF(p, d, c, cpu), _queue((cpu != ANY) ? cpu : ++_next_queue %= CPU::cores()) {};
+
+    const volatile unsigned int & queue() const volatile { return _queue; }
+    void queue(unsigned int q) { _queue = q; }
+    static unsigned int current_queue() { return CPU::id(); }
+
+protected:
+    volatile unsigned int _queue;
+    static volatile unsigned int _next_queue;
 };
 
 __END_SYS
@@ -313,6 +335,10 @@ __BEGIN_UTIL
 template<typename T>
 class Scheduling_Queue<T, GLLF>:
 public Multihead_Scheduling_List<T> {};
+
+template<typename T>
+class Scheduling_Queue<T, PLLF>:
+public Scheduling_Multilist<T> {};
 
 __END_UTIL
 
--- tests/scheduler_llf_test/scheduler_llf_test_traits.h
+++ tests/scheduler_llf_test/scheduler_llf_test_traits.h
@@ -14,7 +14,7 @@ struct Traits<Build> : public Traits_Tokens
     static const unsigned int ARCHITECTURE = RV64;
     static const unsigned int MACHINE = RISCV;
     static const unsigned int MODEL = SiFive_U;
-    static const unsigned int CPUS = 4;
+    static const unsigned int CPUS = 2;
     //static const unsigned int CPUS = 4;
     static const unsigned int NETWORKING = STANDALONE;
     static const unsigned int EXPECTED_SIMULATION_TIME = 60; // s (0 => not simulated)
@@ -143,6 +143,7 @@ struct Traits<Thread> : public Traits<Build>
     static const bool trace = false;
 
     typedef GLLF Criterion;
+	static const unsigned int smp_algorithm = GLOBAL;
     static const unsigned int QUANTUM = 10000; // us
 };
 

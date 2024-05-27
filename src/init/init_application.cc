0a208de050e580f1d46ff8f11b6fc13f1a9420b7
scarpart <aenartzr@gmail.com>
2024-05-27 02:32:54 -0300

    barriers perfeitamente ok

--- src/init/init_application.cc
+++ src/init/init_application.cc
@@ -20,45 +20,42 @@ public:
     {
 		_print("init_app\n");
 
-		db<Thread>(WRN) << "__init_app = Init_Application()" << endl;
-        //CPU::smp_barrier();
+		// Initialize Application's heap
+		db<Init>(INF) << "Initializing application's heap: ";
 
-        if (!CPU::is_bootstrap())
-        {
-            CPU::smp_barrier();
-            return;
-        }
+		if (CPU::is_bootstrap()) 
+		{
+			if (Traits<System>::multiheap)
+			{ 
+				// heap in data segment arranged by SETUP
+				db<Init>(INF) << endl;
+				//
+				// ld is eliminating the data segment in some compilations,
+				// particularly for RISC-V, and placing _end in the code segment
+				char *heap = (MMU::align_page(&_end) >= CPU::Log_Addr(Memory_Map::APP_DATA)) 
+					? MMU::align_page(&_end)
+					: CPU::Log_Addr(Memory_Map::APP_DATA); 
 
-        // Initialize Application's heap
-        db<Init>(INF) << "Initializing application's heap: ";
-        if (Traits<System>::multiheap)
-        { 
-			// heap in data segment arranged by SETUP
-			db<Init>(INF) << endl;
-			//
-			// ld is eliminating the data segment in some compilations,
-			// particularly for RISC-V, and placing _end in the code segment
-            char *heap = (MMU::align_page(&_end) >= CPU::Log_Addr(Memory_Map::APP_DATA)) 
-				? MMU::align_page(&_end)
-				: CPU::Log_Addr(Memory_Map::APP_DATA); 
+				if (Traits<Build>::SMOD != Traits<Build>::KERNEL)
+				{
+					// if not a kernel, then use the stack allocated by SETUP,
+					// otherwise make that part of the heap
+					heap += MMU::align_page(Traits<Application>::STACK_SIZE);
+				}
 
-            if (Traits<Build>::SMOD != Traits<Build>::KERNEL)
-			{
-				// if not a kernel, then use the stack allocated by SETUP,
-				// otherwise make that part of the heap
-                heap += MMU::align_page(Traits<Application>::STACK_SIZE);
+				Application::_heap = new (&Application::_preheap[0]) Heap(heap, HEAP_SIZE);
 			}
+			else
+			{
+				db<Init>(INF) << "adding all free memory to the unified system's heap!" << endl;
+				for (unsigned int frames = MMU::allocable(); frames; frames = MMU::allocable())
+				{
+					System::_heap->free(MMU::alloc(frames), frames * sizeof(MMU::Page));
+				}
+			}
+		}
 
-            Application::_heap = new (&Application::_preheap[0]) Heap(heap, HEAP_SIZE);
-        }
-        else
-        {
-            db<Init>(INF) << "adding all free memory to the unified system's heap!" << endl;
-            for (unsigned int frames = MMU::allocable(); frames; frames = MMU::allocable())
-                System::_heap->free(MMU::alloc(frames), frames * sizeof(MMU::Page));
-        }
-
-        CPU::smp_barrier(); // complete the barrier on line 26
+        CPU::smp_barrier(); 
     }
 };
 
